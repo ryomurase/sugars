c17c989ea803630ed6e3122fe85fb8fd6ddfbc5c
村瀬綾 <>
2019-01-15 13:32:08 +0900

    カテゴリー選択編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,5 @@ products_csv = CSV.readlines("db/products.csv")
 products_csv.shift
 products_csv.each do |row|
   Product.create(name: row[1], detail: row[3], company: row[4], material: row[5], preservation: row[6], quantity: row[9],category_id: row[10], user_id: row[11])
+  #   .create(key: row[1], filename: row[2], content_type: row[3], metadata: row[4], byte_size: row[5], checksum: row[6])
 end
